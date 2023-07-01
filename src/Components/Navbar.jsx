d2a6890955589952ab0cfad94585de55c0bc3930
Fale King Nanmua <kingnanmuafale@gmail.com>
2023-07-01 13:21:14 -0700

    Update Router

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <span style={{marginLeft: 10}}>HOME</span>
+      <span style={{marginLeft: 10}}>ABOUT_US</span>
+      <span style={{marginLeft: 10}}>CONTACT_US</span>
+    </div>
+  )
+}
+
+export default Navbar
